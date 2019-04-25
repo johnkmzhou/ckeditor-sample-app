65615bfe8b209cd91073ddbf55616692d7f11e43
jzhou <john_zhou@hid.uscourts.gov>
2019-04-24 15:28:35 -1000

    Eject project in order to add custom plugins.

--- src/App.js
+++ src/App.js
@@ -10,21 +10,20 @@ import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic'
 import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote'
 import List from '@ckeditor/ckeditor5-list/src/list'
 import Link from '@ckeditor/ckeditor5-link/src/link'
-//import Placeholder from "ckeditor5-placeholder";
+import Placeholder from "ckeditor5-placeholder";
 
 import './App.css';
 
 const App = () => {
   const [editorData, setEditorData] = useState("<p>Hello from CKEditor 5!</p>");
-
+// bulleted list is missing
   return (
     <div className="App">
       <h2>Using CKEditor 5 build in React</h2>
       <CKEditor
         config={{
-          plugins: [Essentials, Heading, Paragraph, Bold, Italic, BlockQuote, Alignment, List, Link],
-          toolbar: ['Heading', '|', 'Bold', 'Italic', 'Alignment', 'List', 'ListUI', 'BlockQuote', 'Undo', 'Redo'],
-          removePlugins: ['Image', 'ImageCaption', 'ImageStyle', 'ImageToolbar', 'ImageUpload']
+          plugins: [Essentials, Heading, Paragraph, Bold, Italic, BlockQuote, Alignment, List, Link, Placeholder],
+          toolbar: ['Heading', '|', 'Bold', 'Italic', 'Alignment', 'BulletedList', 'NumberedList', 'BlockQuote', 'Link', 'Undo', 'Redo', 'Placeholder']
         }}
         editor={InlineEditor}
         data={editorData}
